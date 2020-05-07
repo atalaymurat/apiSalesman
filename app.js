a589cb06dbac879ef39d2cec669750c2f965ab00
mAta <atalaymurat@msn.com>
2020-05-07 18:51:45 +0300

    mongoose setup

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const credentials = require('./credentials')
 
+// Mongo DB Settings
+const mongoose = require('mongoose')
+let opts = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+switch (app.get('env')) {
+  case 'development':
+    mongoose.connect(credentials.mongo.development.connect, opts)
+    break
+  case 'production':
+    mongoose.connect(credentials.mongo.production.connect, opts)
+    break
+  default:
+    throw new Error('Unkown execution enviroment: ' + app.get('env'))
+}
+var db = mongoose.connection
+db.once('open', function () {
+  console.log('Db mongo connection success')
+})
+
+// Routes
 app.get('/', (req, res) => {
-  res.status(200)
   res.send('Salesman Api index')
 })
 
@@ -18,18 +39,18 @@ switch (app.get('env')) {
 }
 
 // 404 catch-all handler (middleware)
-app.use(function (req, res, next) {
-  res.status(404)
-  res.send('404')
+app.use((req, res, next) => {
+  res.status(404).send('404')
 })
 
 // 500 error handler (middleware)
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500)
   res.send('500')
 })
 
+const port = 4000
 app.listen(port, () =>
   console.log('App started in mode ' + app.get('env') + ` listening at http://localhost:${port}`)
 )
--- readMe.md
+++ readMe.md
@@ -1,1 +1,4 @@
 **Node js Project with Express for React
+
+you need a .credentials.js file to configure your keys for 
+database, mail services
